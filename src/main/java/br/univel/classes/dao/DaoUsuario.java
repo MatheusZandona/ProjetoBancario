a7341085ddb4cfa252bd037e64e4b8ec3d65c26c
MatheusZandona <zan_matheus@yahoo.com.br>
2016-09-06 19:39:18 -0300

    Acerto de metodos.

--- src/main/java/br/univel/classes/dao/DaoUsuario.java
+++ src/main/java/br/univel/classes/dao/DaoUsuario.java
@@ -1,11 +1,13 @@
 package br.univel.classes.dao;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.mysql.jdbc.PreparedStatement;
 
 import br.univel.classes.Agencia;
+import br.univel.classes.Conta;
 import br.univel.classes.Pessoa;
 import br.univel.classes.Usuario;
 import br.univel.classes.bd.ConexaoBD;
@@ -66,26 +68,78 @@ public class DaoUsuario implements Dao<Usuario, Integer>{
 
 	@Override
 	public void atualizar(Usuario t) {
-		// TODO Auto-generated method stub
-		
+		try {
+			PreparedStatement ps = (PreparedStatement) ConexaoBD.getInstance().abrirConexao()
+					.clientPrepareStatement("UPDATE USUARIO SET NOME = ?, SENHA = ?, PESSOA = ? WHERE ID = ?");
+			
+			ps.setString(1, t.getUser());
+			ps.setString(3, t.getSenha());
+			ps.setInt(2, t.getPessoa().getId());
+			
+			ps.executeUpdate();
+			ps.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void excluir(Integer k) {
-		// TODO Auto-generated method stub
-		
+		try {
+			PreparedStatement ps = (PreparedStatement) ConexaoBD.getInstance().abrirConexao()
+					.clientPrepareStatement("DELETE FROM USUARIO WHERE ID = ?");
+			
+			ps.setInt(1, k);
+			
+			ps.executeUpdate();
+			ps.close();
+		} catch (Exception e) {
+			e.getMessage();
+		}
 	}
 
 	@Override
 	public List<Usuario> listarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Usuario> usuarios = new ArrayList<>();
+		DaoPessoa daoPessoa = new DaoPessoa();
+		Pessoa pessoa;
+		try {
+			PreparedStatement ps = (PreparedStatement) ConexaoBD.getInstance().abrirConexao()
+									.clientPrepareStatement("SELECT * FROM USUARIO");
+			ResultSet result =  ps.executeQuery();
+			while(result.next()){
+				Usuario us = new Usuario();
+				us.setId(result.getInt("id"));
+				us.setUser(result.getString("nome"));
+				if(daoPessoa.buscar(Integer.parseInt(Funcoes.removerCaracteresEspeciais(result.getString("pessoa")))) != null){
+					pessoa = new Pessoa();
+					pessoa = daoPessoa.buscar(Integer.parseInt(Funcoes.removerCaracteresEspeciais(result.getString("pessoa"))));
+					us.setPessoa(pessoa);
+				}
+				us.setSenha(result.getString("senha"));
+				usuarios.add(us);
+			}
+			
+			ps.close();
+			result.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return usuarios;
 	}
 
 	@Override
 	public int proximoID() {
-		// TODO Auto-generated method stub
-		return 0;
+		int proxId = 0;
+		try {
+			PreparedStatement ps = (PreparedStatement) ConexaoBD.getInstance().abrirConexao()
+					.clientPrepareStatement("SELECT MAX(ID) FROM USUARIO");
+			ResultSet result = ps.executeQuery();
+			proxId = 1 + result.getInt("id");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return proxId;
 	}
 
 }
