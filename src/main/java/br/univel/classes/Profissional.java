f1845daaed58e5d8d64bf9d1af0cead23a065f06
LucasMedeiros10 <lucas_medeiros09@hotmail.com>
2016-09-10 16:08:07 -0300

    classe profissional

---
+++ src/main/java/br/univel/classes/Profissional.java
@@ -0,0 +1,15 @@
+package br.univel.classes;
+
+public class Profissional extends Pessoa{
+	
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+}
